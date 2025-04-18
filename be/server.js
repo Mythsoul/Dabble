e9b32bcdb9f68e56b673b63a7bdb04823b63664e
mythsoul <mythsoul2023@gmail.com>
2025-04-18 20:24:50 +0545

    database setuped

--- be/server.js
+++ be/server.js
@@ -1,5 +1,5 @@
 import express from "express" 
-
+import { Database } from "./config/db/db.js";
 
 const app = express(); 
 const port = 3000; 
@@ -7,13 +7,14 @@ const port = 3000;
 
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true })) 
+app.use(express.static("public")) 
 
 
 app.get("/" , (req , res)=>{ 
     res.send('hello the server is running ')
 })
 
+
 app.listen(port , ()=>{ 
     console.log("The server is running at port " , port)
 }); 
-
